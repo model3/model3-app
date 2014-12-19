d7dc1a243957db747ce577d707c411d894cd68cc
hectorbenitez <hbenitez@nearsoft.com>
2014-12-18 22:32:00 -0600

    Moving to src

--- public/index.php
+++ public/index.php
@@ -1,20 +1,15 @@
 <?php
 
 /**
- * Usar esta seccion solo en desarrollo para debug
+ * Only DEBUG
  */
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', true);
+
 date_default_timezone_set('America/Mexico_City');
 
-set_include_path('../library/' . PATH_SEPARATOR . get_include_path());
-set_include_path('../library/Doctrine/lib/'.PATH_SEPARATOR.get_include_path());
-set_include_path('../application/' . PATH_SEPARATOR . get_include_path());
-set_include_path('../application/Controller/' . PATH_SEPARATOR . get_include_path());
-set_include_path('../application/Model/' . PATH_SEPARATOR . get_include_path());
+require_once __DIR__.'/../vendor/autoload.php';
 
-require_once('Model3/Loader.php');
-Model3_Loader::registerAutoload();
 //inicio de la aplicacion
-Model3_Site::initSite('../application/Config/config.ini');
-Model3_Site::dispatch(new Model3_Request);
+Model3\Site\Site::initSite('../application/Config/config.ini');
+Model3\Site\Site::dispatch(new Model3_Request);
